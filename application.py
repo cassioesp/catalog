d9618db305dee6a1b567d0560b43f6fb63b810a9
vagrant <vagrant@vagrant.vm>
2018-03-09 01:56:02 +0000

    creating templates and routes

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -23,28 +23,33 @@ def categoriesJSON():
 @app.route('/')
 @app.route('/catalog/')
 def showCategories():
-    return "This page will show all my categories"
+    return render_template('catalog.html')
 
 
-@app.route('/catalog/<int:category_id>')
-@app.route('/catalog/<int:category_id>/item')
-def showMenu(category_id):
-    return "This page is the item for category %s" % category_id
+@app.route('/catalog/<string:category>/')
+@app.route('/catalog/<string:category>/items/')
+def showItems(category):
+    return render_template('items.html')
+
+
+@app.route('/catalog/<string:category>/<string:item>/')
+def showItem(item):
+    return render_template('itemDescription.html')
 
 
 @app.route('/catalog/<int:category_id>/item/new')
 def newItem(category_id):
-    return "This page is for making a new item for category %s" % category_id
+    return render_template('newItem.html')
 
 
-@app.route('/catalog/<int:category_id>/item/<int:menu_id>/edit')
-def editItem(category_id, menu_id):
-    return "This page is for editing item %s" % menu_id
+@app.route('/catalog/<string:item>/edit')
+def editItem(item):
+    return render_template('editItem.html')
 
 
-@app.route('/catalog/<int:category_id>/item/<int:menu_id>/delete')
-def deleteItem(category_id, menu_id):
-    return "This page is for deleting item %s" % menu_id
+@app.route('/catalog/<string:item>/delete')
+def deleteItem(item):
+    return render_template('deleteItem.html')
 
 
 if __name__ == '__main__':
